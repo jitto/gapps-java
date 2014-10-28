6e9696653cb94a5d7d313bb2a1a625498793aeb5
jitto <justine.vt@gmail.com>
2014-10-28 13:39:15 +0000

    Add jetty, fix get profiles

--- src/main/java/com/ashiyana/rest/ProfileResource.java
+++ src/main/java/com/ashiyana/rest/ProfileResource.java
@@ -1,15 +1,14 @@
 package com.ashiyana.rest;
 
 import javax.ws.rs.*;
-import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Collections;
 
 @Path("/profile")
 public class ProfileResource {
     @GET
     @Produces("application/json")
-    public String getMany() {
+    public List<Profile> getMany() {
         Profile newProfile = new Profile();
         newProfile.name = "test";
         return Collections.singletonList(newProfile);
